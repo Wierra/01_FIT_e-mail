0d1da03fd509fadb4dd44f20f9b086de0e7c6461
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-28 23:05:14 +0100

    Uložení úkolu 28.10.

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const jmeno = prompt("Vítejte v IS Fakulty Informačních technilogií ČVUT. P
 const prijmeni = prompt("Vyplň své příjmení:").toLowerCase().trim()
 const domena = "fit.cvut.cz"
 
-document.body.innerHTML += `<p>Vyplněné jméno: " + jmeno + " " + prijmeni + " " + "</p>`
-document.body.innerHTML += `<p>Vytvořená e-mailová adresa: ${jmeno.slice(0, 3)}${prijmeni.slice(0, 5)}` + `@` + domena + `</p>`
+document.body.innerHTML += "<p>Vyplněné jméno: " + jmeno + " " + prijmeni + " " + "</p>"
+document.body.innerHTML += `<p>Tvá e-mailová adresa pro přihlašování do našeho IS: <h1>${jmeno.slice(0, 3)}${prijmeni.slice(0, 5)}` + `@` + domena + `</h1></p>`
 
 //trim = žádné bílé znaky na začátku ani na konci
-//
+//slice = vyříznutí znaků - první tři/pět
+//bez diakritiky
